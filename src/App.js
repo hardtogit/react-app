60a2a2f090a9061d48c255454042cb273acf58f6
xiangguo <xiangguo@bao.cn>
2018-11-05 21:23:42 +0800

    集成相关功能

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import {Provider} from 'react-redux'
 import {Router,Redirect,Route,IndexRoute,browserHistory} from 'react-router'
 import { createStore,applyMiddleware,compose} from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { syncHistoryWithStore ,routerMiddleware} from 'react-router-redux';
 import RootComponent from './compnents/common/RootComponent'
 import reducer from './store/reducers'
 import './App.css';
 
 import Home from './pages/Home'
 import Find from './pages/Find'
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store=createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store=createStore(reducer,composeEnhancers(applyMiddleware(routerMiddleware(browserHistory))))
 const history = syncHistoryWithStore(browserHistory, store);
 
 class App extends Component {
